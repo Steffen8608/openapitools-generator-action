d7710c67918572cc5a330431f81684bff606c509
Steffen Glaeser <steffen.glaeser@transfermarkt.de>
2024-11-17 22:19:15 +0100

    make action usable for gitea

--- entrypoint.py
+++ entrypoint.py
@@ -2,27 +2,27 @@ from subprocess import call
 from sys import argv
 from os import getenv, getuid
 
-(_, generator, docker_repository, docker_image, generator_tag, openapi_file, openapi_url, config_file, template_dir, *args) = argv
+(_, workspace, generator, docker_repository, docker_image, generator_tag, openapi_file, openapi_url, config_file, template_dir, *args) = argv
 
-cmd = f"docker run -u {getuid()}:1001 --rm --workdir /github/workspace -v {getenv('GITHUB_WORKSPACE')}:/github/workspace"
+cmd = f"docker run -u {getuid()}:1001 --rm --workdir {workspace} -v {getenv('GITHUB_WORKSPACE')}:{workspace}"
 cmd = f"{cmd} {docker_repository}/{docker_image}:{generator_tag} generate"
-cmd = f"{cmd} -g {generator} -o /github/workspace/{generator}-client"
+cmd = f"{cmd} -g {generator} -o {workspace}/{generator}-client"
 
 if openapi_url == "UNSET":
     if not openapi_file.startswith("/"):
-        openapi_file = f"/github/workspace/{openapi_file}"
+        openapi_file = f"{workspace}/{openapi_file}"
     cmd = f"{cmd} -i {openapi_file}"
 else:
     cmd = f"{cmd} -i {openapi_url}"
 
 if config_file != "UNSET":
     if not config_file.startswith("/"):
-        config_file = f"/github/workspace/{config_file}"
+        config_file = f"{workspace}/{config_file}"
     cmd = f"{cmd} -c {config_file}"
 
 if template_dir != "UNSET":
     if not template_dir.startswith("/"):
-        template_dir = f"/github/workspace/{template_dir}"
+        template_dir = f"{workspace}/{template_dir}"
     cmd = f"{cmd} -t {template_dir}"
 
 if args:
